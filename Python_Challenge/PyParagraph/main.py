8b13b0fb9675a4501e513f84a925f3a25bca340c
Suresh Adda <asureshb@gmail.com>
2018-03-01 12:36:51 -0800

    PyParagraph application to display text metrics of given paragraph

---
+++ Python_Challenge/PyParagraph/main.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python
+
+#
+#  PyBoss/main.py is to format employee records
+#  Syntax: python PyParagraph/main.py <csv_file_path> <output_file_path>
+#
+
+#import all required modules here
+import re
+import sys
+
+def textAnalytics(text_file):
+    with open(text_file, 'r') as fd:
+        text = fd.read()
+
+    sentences  = re.split(r' *[\.\?!][\'"\)\]]* +', text)
+    words = re.findall(r'\w+', text)
+
+    print("Number of sentences: " + str(len(sentences)))
+    print("Number of words: " + str(len(words)))
+
+    # calculate average letter count
+    char_count = 0
+    for w in words:
+        char_count += len(w)
+    print("Average letter count: %s" % str(char_count / len(words)))
+
+    # calculate average length of sentence
+    sentence_count = 0
+    for s in sentences:
+        sentence_count += len(s)
+    print("Average sentence length: %s" % str(sentence_count / len(sentences)))
+
+if __name__ == '__main__':
+    textAnalytics(sys.argv[1])
